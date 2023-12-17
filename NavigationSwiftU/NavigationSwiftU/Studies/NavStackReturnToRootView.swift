3983e1e509c3a7111f2d48ee03bafe10f99b3f10
OLEKSANDR ISAIEV <nevkluchat@gmail.com>
2023-12-17 09:33:28 +0200

    add new class for save path

--- NavigationSwiftU/NavigationSwiftU/Studies/NavStackReturnToRootView.swift
+++ NavigationSwiftU/NavigationSwiftU/Studies/NavStackReturnToRootView.swift
@@ -7,6 +7,35 @@
 
 import SwiftUI
 
+@Observable
+class PathStore {
+    var path: [Int] {
+        didSet {
+            
+        }
+    }
+    
+    private let savePath = URL.documentsDirectory.appending(path: "SavedPath")
+    
+    init() {
+        if let data = try? Data(contentsOf: savePath) {
+            if let decoded = try? JSONDecoder().decode([Int].self, from: data) {
+                path = decoded
+                return
+            }
+        }
+        path = []
+    }
+    
+    func save() {
+        do {
+            let data = try JSONEncoder().encode(path)
+            try data.write(to: savePath)
+        } catch {
+            print("Failed to save navigation data")
+        }
+    }
+}
 struct DetailNavView: View {
     var number: Int
     @Binding var path: NavigationPath
