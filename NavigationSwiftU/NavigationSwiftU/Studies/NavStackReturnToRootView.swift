7591c70055bc974e71e718954723683e3ab96d79
OLEKSANDR ISAIEV <nevkluchat@gmail.com>
2023-12-17 09:25:57 +0200

    replace int with navigation path

--- NavigationSwiftU/NavigationSwiftU/Studies/NavStackReturnToRootView.swift
+++ NavigationSwiftU/NavigationSwiftU/Studies/NavStackReturnToRootView.swift
@@ -9,20 +9,26 @@ import SwiftUI
 
 struct DetailNavView: View {
     var number: Int
+    @Binding var path: NavigationPath
     
     var body: some View {
         NavigationLink("Go to random number", value: Int.random(in: 1...1000))
             .navigationTitle("Number \(number)")
+            .toolbar {
+                Button("Home") {
+                    path = NavigationPath()
+                }
+            }
     }
 }
 struct NavStackReturnToRootView: View {
-    @State private var path = [Int]()
+    @State private var path = NavigationPath()
     
     var body: some View {
         NavigationStack(path: $path) {
-            DetailNavView(number: 0)
+            DetailNavView(number: 0, path: $path)
                 .navigationDestination(for: Int.self) { i in
-                    DetailNavView(number: i)
+                    DetailNavView(number: i, path: $path)
                 }
         }
     }
